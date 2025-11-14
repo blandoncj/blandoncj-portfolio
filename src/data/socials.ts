dfbe7134bc4cf8825903402b280cc06cdf800d71
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-11-14 10:15:47 -0500

    feat: add SocialLink interface and initial social links data

---
+++ src/data/socials.ts
@@ -0,0 +1,23 @@
+export interface SocialLink {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+export const socials: SocialLink[] = [
+  {
+    name: "GitHub",
+    url: "https://github.com/blandoncj",
+    icon: "Github",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com/in/blandoncj",
+    icon: "Linkedin",
+  },
+  {
+    name: "Gmail",
+    url: "mailto:jacoboblandon94@gmail.com",
+    icon: "Mail",
+  },
+];
